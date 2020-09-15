d789d620784c14202428a6b5b56fd6af74c157c8
Kael Steinert <kaelsteinert@Kaels-Mac-mini.local>
2020-09-15 11:45:46 -0400

    removed live firestore db credentials

--- src/util/db.js
+++ src/util/db.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const configOptions = {
-  apiKey: "AIzaSyAONPC5ZYM6ed-W5qlVxYPzfa7oY-aQIDk",
-  authDomain: "budbuddy-232c4.firebaseapp.com",
-  databaseURL: "https://budbuddy-232c4.firebaseio.com",
-  projectId: "budbuddy-232c4",
-  storageBucket: "gs://budbuddy-232c4.appspot.com",
-  messagingSenderId: "475467948845"
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://PROJECT_ID.firebaseio.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID"
 };
 
 // Get a Firestore instance
